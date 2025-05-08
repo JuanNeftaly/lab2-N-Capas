bd7a8b1285107f5d495a6185d575c3e435114ac0
JuanNeftaly <00182222@uca.edu.sv>
2025-05-08 17:06:06 -0600

    Add: Departamento Entity :fire:

--- innova/src/main/java/Models/Entities/Departamento.java
+++ innova/src/main/java/Models/Entities/Departamento.java
@@ -9,34 +9,8 @@ public class Departamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nombre;
 
     @OneToMany(mappedBy = "departamento")
     private Set<Empleado> empleados = new HashSet<>();
-
-    // Getters y setters// Getters y setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Set<Empleado> getEmpleados() {
-        return empleados;
-    }
-
-    public void setEmpleados(Set<Empleado> empleados) {
-        this.empleados = empleados;
-    }
 }
