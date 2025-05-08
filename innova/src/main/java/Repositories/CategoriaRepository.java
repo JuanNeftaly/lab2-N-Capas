b4d0939a5c98f1938ca3e1734deba68ad2332706
JuanNeftaly <00182222@uca.edu.sv>
2025-05-08 17:56:20 -0600

    Add: Repository of each entity :sparkles:

---
+++ innova/src/main/java/Repositories/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package Repositories;
+
+import Models.Entities.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
+
