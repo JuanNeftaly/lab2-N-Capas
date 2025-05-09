0ba471876d86283b974c490aedb3aa6fdeec2615
Adriel Escobar <00075522@uca.edu.sv>
2025-05-08 21:55:55 -0600

    :sparkles:Implementacion de todos los services

--- innova/src/main/java/Services/DepartamentoService.java
+++ innova/src/main/java/Services/DepartamentoService.java
@@ -1,11 +1,15 @@
 package Services;
 
 import Models.Entities.Departamento;
+import Models.Entities.Empleado;
 import Repositories.DepartamentoRepository;
+import Repositories.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class DepartamentoService {
@@ -13,11 +17,59 @@ public class DepartamentoService {
     @Autowired
     private DepartamentoRepository departamentoRepository;
 
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+    // Obtener todos los departamentos
     public List<Departamento> getAllDepartamentos() {
         return departamentoRepository.findAll();
     }
 
-    public Departamento saveDepartamento(Departamento departamento) {
+    // Obtener departamento por ID
+    public Optional<Departamento> getDepartamentoById(Long id) {
+        return departamentoRepository.findById(id);
+    }
+
+    // Crear un nuevo departamento
+    public Departamento createDepartamento(Departamento departamento) {
         return departamentoRepository.save(departamento);
     }
+
+    // Eliminar un departamento (si no tiene empleados)
+    public boolean deleteDepartamento(Long id) {
+        Optional<Departamento> deptoOpt = departamentoRepository.findById(id);
+        if (deptoOpt.isPresent() && deptoOpt.get().getEmpleados().isEmpty()) {
+            departamentoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    // Agregar un nuevo empleado al departamento
+    public Optional<Empleado> agregarEmpleado(Long deptoId, Empleado nuevoEmpleado) {
+        return departamentoRepository.findById(deptoId).map(departamento -> {
+            nuevoEmpleado.setDepartamento(departamento);
+            return empleadoRepository.save(nuevoEmpleado);
+        });
+    }
+
+    // Reasignar un empleado a otro departamento
+    public Optional<Empleado> reasignarEmpleado(Long deptoId, Long empleadoId) {
+        Optional<Departamento> nuevoDeptoOpt = departamentoRepository.findById(deptoId);
+        Optional<Empleado> empleadoOpt = empleadoRepository.findById(empleadoId);
+
+        if (nuevoDeptoOpt.isPresent() && empleadoOpt.isPresent()) {
+            Empleado empleado = empleadoOpt.get();
+            empleado.setDepartamento(nuevoDeptoOpt.get());
+            return Optional.of(empleadoRepository.save(empleado));
+        }
+        return Optional.empty();
+    }
+
+    // Obtener empleados de un departamento
+    public Optional<List<Empleado>> getEmpleadosPorDepartamento(Long deptoId) {
+        return departamentoRepository.findById(deptoId)
+                .map(departamento -> List.copyOf(departamento.getEmpleados()));
+    }
+
 }
