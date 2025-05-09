5bcabbe8be77c29090c4dc3ca79c62d5ac479236
JuanNeftaly <00182222@uca.edu.sv>
2025-05-09 14:26:04 -0600

    Innova Project :white_check_mark:

--- innova/src/main/java/com/innova/InnovaApplication.java
+++ innova/src/main/java/com/innova/InnovaApplication.java
@@ -3,7 +3,7 @@ package com.innova;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "Models")
+@SpringBootApplication
 public class InnovaApplication {
 
 	public static void main(String[] args) {
